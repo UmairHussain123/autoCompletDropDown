56c0220d18a4f06b6bb131f610976a8c4477b0e3
UmairHussain123 <umair.umair2d@gmail.com>
2022-07-31 15:50:14 +0500

    Update App.js

--- App.js
+++ App.js
@@ -8,44 +8,45 @@ import {
   ScrollView,
 } from "react-native";
 import { Button, TextInput, List } from "react-native-paper";
-import { AutocompleteDropdown } from "react-native-autocomplete-dropdown";
 
 import React, { useEffect, useState } from "react";
 import { Leave_type, LeaveReson } from "./assets/data";
 
 export default function App() {
-  useEffect(() => {
-    console.log("asdkfj");
-  }, []);
-  const fruits = [
-    "apple pine",
-    "apple",
-    "aapples",
-    "mango",
-    "banana",
-    "orange",
-    "appless",
-    "pears",
-    "citrusStone",
-    "fruitTropical",
-    "berries",
-    "melons",
-  ];
+  // useEffect(() => {
+  //   console.log("asdkfj");
+  // }, []);
+
   const [arr, setArr] = useState(null);
+  let leaveType;
   const arryFilter = (str) => {
-    if (arr == null) {
-      setArr(fruits);
+    if (arr == null || !str) {
+      setArr(Leave_type);
     } else {
-      const fruitFilter = fruits.filter((fruits) => {
-        return !fruits.indexOf(str);
+      leaveType = Leave_type.filter((leaveType) => {
+        // console.log("ret", !leaveType.title.indexOf(str));
+        return !leaveType.title.indexOf(str);
       });
-      setArr(fruitFilter);
-      console.log(fruitFilter);
+      setArr(leaveType);
     }
+    // console.log("leaveTypes", leaveType);
+
+    // if (arr == null) {
+    //   setArr(fruits);
+    // } else {
+    //   const fruitFilter = fruits.filter((fruits) => {
+    //     // return !fruits.indexOf(str);
+    //     console.log(!fruits.title.indexOf(str));
+    //   });
+    //   setArr(fruitFilter);
+    //   console.log(fruitFilter);
+    // }
   };
-  // arryFilter("a");
-  const [fruitVal, setFruitVal] = useState("");
-  const [fruitFlag, setFruitFlag] = useState(false);
+  // arryFilter("SA Days");
+
+  // console.log();
+  const [leaveVal, setLeaveVal] = useState("");
+  const [leaveFlag, setFruitFlag] = useState(false);
   return (
     <View style={styles.container}>
       <View
@@ -59,13 +60,13 @@ export default function App() {
         <TextInput
           label="search fruit"
           editable
-          value={fruitVal}
+          value={leaveVal}
           style={{ width: 300, height: 50 }}
           right={<TextInput.Icon name="chevron-down" />}
           onChangeText={(text) => {
-            setFruitVal(text);
+            setLeaveVal(text);
             setFruitFlag(true);
-            arryFilter(text);
+            arryFilter(text.toUpperCase());
           }}
           onFocus={() => {
             setFruitFlag(true);
@@ -75,44 +76,48 @@ export default function App() {
             setFruitFlag(false);
           }}
         />
-        {/* <ScrollView style={{height:500}}> */}
 
         <View style={{ position: "absolute", top: "15%", height: 100 }}>
           <ScrollView style={{}} keyboardShouldPersistTaps={true}>
-            {fruitFlag
+            {leaveFlag
               ? arr.map((item, index) => {
                   return (
                     <View
                       key={index}
                       style={{
-                        // borderBottomWidth: 1,
-                        // zIndex: 1000,
                         width: 300,
-                        // marginTop: 5,
+                        borderRadius: 1,
                         padding: 5,
-                        elevation: 3,
-                        // position: "absolute",
-                        // top: 100,
+                        elevation: 1,
                       }}
                     >
-                      <TouchableOpacity
+                      <Button
                         onPress={() => {
-                          setFruitVal(item);
+                          setLeaveVal(item.title);
                           setFruitFlag(false);
                         }}
+                        color="black"
+                        style={{ alignContent: "flex-start" }}
+                      >
+                        {item.title}
+                      </Button>
+                      {/* <TouchableOpacity
+                        onPress={() => {
+                          setLeaveVal(item.title);
+                          setFruitFlag(false);
+                        }}
+                        style={{ flex: 1 }}
                       >
                         <View>
-                          <Text key={index}>{item}</Text>
+                          <Text key={index}>{item.title}</Text>
                         </View>
-                      </TouchableOpacity>
+                      </TouchableOpacity> */}
                     </View>
                   );
                 })
               : null}
           </ScrollView>
         </View>
-        {/* </ScrollView> */}
-        {/* <Text>Apple</Text> */}
       </View>
       <TextInput
         label="search fruit"
